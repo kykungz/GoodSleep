01e15afabceb0c36479588ea453805b393d1a18e
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-05-24 15:45:22 +0700

    :tada: Initial commit

---
+++ app/src/main/java/com/example/kongpon_macbook/goodsleep/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.kongpon_macbook.goodsleep
+
+import android.app.Activity
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.leinardi.android.speeddial.SpeedDialActionItem
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val PREF_NAME = "good_sleep_pref"
+        val TIME_JSON_KEY = "time_json"
+        val TYPE_JSON_KEY = "type_json"
+    }
+
+    private lateinit var time: String
+    private lateinit var type: String
+
+    private fun configureHome(type: String, time: String) {
+        when (type) {
+            "" -> textView.text = "You have no timer!"
+            TimePicker.WAKE -> {
+                textView.text = "You must go to bed at ${time}\n\nIt takes the average human about 15 minutes to fall asleep.\n"
+                textView.setTextColor(Color.BLACK)
+                home.setBackgroundResource(R.drawable.town_day)
+            }
+            TimePicker.SLEEP -> {
+                textView.text = "You must wake up at ${time}.\n\nIf you wake up at this time, you'll rise in between 90-minute sleep cycles. A good night's sleep consists of 5-6 complete sleep cycles."
+                textView.setTextColor(Color.WHITE)
+                home.setBackgroundResource(R.drawable.town_night)
+            }
+        }
+        if (time.equals("")) {
+            textView.text = "You have no timer!"
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val pref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        type = pref.getString(TYPE_JSON_KEY,"")
+        time = pref.getString(TIME_JSON_KEY,"")
+        configureHome(type, time)
+
+        speedDial.addActionItem(SpeedDialActionItem.Builder(R.id.fab_sleep_action,R.drawable.ic_moon_24dp)
+                .setLabel("If I go to bed at ...")
+                .setLabelColor(Color.BLACK)
+                .create()
+        )
+        speedDial.addActionItem(SpeedDialActionItem.Builder(R.id.fab_wake_action,R.drawable.ic_sun_24dp)
+                .setLabel("If I want to get up at ...")
+                .setLabelColor(Color.BLACK)
+                .setFabBackgroundColor(Color.parseColor("#FFA500"))
+                .create()
+        )
+        speedDial.setOnActionSelectedListener { actionItem ->
+            when (actionItem.id) {
+                R.id.fab_sleep_action -> {
+                    val onTimeSet = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        Toast.makeText(this, "set ${hourOfDay} ${minute}", Toast.LENGTH_SHORT).show()
+                        println("set ${hourOfDay} ${minute}")
+                        val intent = Intent(this, TimePicker::class.java)
+                        intent.putExtra(TimePicker.TYPE, TimePicker.SLEEP)
+                        intent.putExtra(TimePicker.HOUR, hourOfDay)
+                        intent.putExtra(TimePicker.MINUTE, minute)
+                        startActivityForResult(intent, TimePicker.REQUEST_CODE)
+                    }
+                    val timePicker = TimePickerDialog(
+                            this,
+                            onTimeSet,
+                            Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                            Calendar.getInstance().get(Calendar.MINUTE),
+                            false
+                    )
+                    timePicker.show()
+                }
+                R.id.fab_wake_action -> {
+                    val onTimeSet = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                        Toast.makeText(this, "set ${hourOfDay} ${minute}", Toast.LENGTH_SHORT).show()
+                        println("set ${hourOfDay} ${minute}")
+                        val intent = Intent(this, TimePicker::class.java)
+                        intent.putExtra(TimePicker.TYPE, TimePicker.WAKE)
+                        intent.putExtra(TimePicker.HOUR, hourOfDay)
+                        intent.putExtra(TimePicker.MINUTE, minute)
+                        startActivityForResult(intent, TimePicker.REQUEST_CODE)
+                    }
+                    val timePicker = TimePickerDialog(
+                            this,
+                            onTimeSet,
+                            8,
+                            0,
+                            false
+                    )
+                    timePicker.show()
+                }
+            }
+            false
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TimePicker.REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            time = data.getStringExtra(TimePicker.TIME)
+            type = data.getStringExtra(TimePicker.TYPE)
+            configureHome(type, time)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val pref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        with (pref.edit()) {
+            this.putString(TIME_JSON_KEY, time)
+            this.putString(TYPE_JSON_KEY, type)
+            this.apply()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kongpon_macbook/goodsleep/TimePicker.kt
@@ -0,0 +1,91 @@
+package com.example.kongpon_macbook.goodsleep
+
+import android.app.Activity
+import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_time_picker.*
+import net.danlew.android.joda.JodaTimeAndroid
+import org.joda.time.DateTime
+import org.joda.time.LocalDate
+import org.joda.time.MutableDateTime
+import java.util.*
+import org.joda.time.format.DateTimeFormat
+
+
+
+class TimePicker : AppCompatActivity() {
+    private val formatter = DateTimeFormat.forPattern("hh:mm a")
+    private lateinit var target: DateTime
+
+    companion object {
+        val REQUEST_CODE = 123
+        val TYPE = "timePicker_type"
+        val WAKE = "timePicker_wake"
+        val SLEEP = "timePicker_sleep"
+        val HOUR = "timePicker_hour"
+        val MINUTE = "timePicker_minute"
+        val TIME = "timePicker_time"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_picker)
+        JodaTimeAndroid.init(this)
+        val type = intent.getStringExtra(TYPE)
+        val temp = MutableDateTime()
+        temp.hourOfDay = intent.getIntExtra(HOUR, 0)
+        temp.minuteOfHour = intent.getIntExtra(MINUTE, 0)
+        target = DateTime(temp)
+        when (type) {
+            WAKE -> time.setTextColor(ContextCompat.getColor(this, android.R.color.holo_orange_dark))
+            SLEEP -> time.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        }
+        time.text = target.toString(formatter)
+
+        when (intent.getStringExtra(TYPE)) {
+            WAKE -> configureWake()
+            SLEEP -> configureSleep()
+        }
+    }
+
+    private fun configureWake() {
+        val buttons = listOf<Button>(time1, time2, time3, time4, time5, time6).asReversed()
+        for ((index, time) in buttons.withIndex()) {
+            val recommended = target.minusMinutes(15 + 90 * (index + 1))
+            println(target.toString(DateTimeFormat.forPattern("hh:mm a")))
+            time.text = recommended.toString(formatter)
+            time.setOnClickListener(View.OnClickListener { v ->
+                val intent = Intent()
+                intent.putExtra(TIME, recommended.toString(formatter))
+                intent.putExtra(TYPE, WAKE)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            })
+        }
+        text1.text = "If you want to wake up at"
+        text2.text = "You must sleep at one of the following times:"
+    }
+
+    private fun configureSleep() {
+        val buttons = listOf<Button>(time1, time2, time3, time4, time5, time6)
+        for ((index, time) in buttons.withIndex()) {
+            val recommended = DateTime().plusMinutes(15 + 90 * (index + 1))
+            time.text = recommended.toString(formatter)
+            time.setOnClickListener(View.OnClickListener { v ->
+                val intent = Intent()
+                intent.putExtra(TIME, recommended.toString(formatter))
+                intent.putExtra(TYPE, SLEEP)
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            })
+        }
+        text1.text = "If you go to bed at"
+        text2.text = "You should try to get up at one of the following times:"
+    }
+}
